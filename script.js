58978b4051852700ac70ea6151b40966ec151f44
Dhaffa Ardhana <ardhanadhaffa@gmail.com>
2021-01-18 16:12:46 +0700

    Fixed js

--- script.js
+++ script.js
@@ -13,39 +13,30 @@ document.addEventListener("DOMContentLoaded", () => {
 	let lists = document.getElementById('lists')
 
 	// Functions
-	function add(input) {
-		let li = document.createElement('li')
-		let div = document.createElement('div')
-		let img_check = document.createElement('img')
-		let p = document.createElement('p')
-		let img_delete = document.createElement('img')
-		img_check.classList.add('check')
-		img_delete.classList.add('delete')
-		lists.appendChild(li)
-		li.appendChild(div)
-		div.appendChild(img_check)
-		div.appendChild(p)
-		li.appendChild(img_delete)
-		img_check.src = 'img/check.png'
-		img_check.alt = 'Check logo'
-		img_delete.src = 'img/trash-can.png'
-		img_delete.alt = 'Trash logo'
-		if (!input) {
-			if (text.value) {
-				let text_in = document.createTextNode(text.value)
-				p.appendChild(text_in)
-				text.value = ''
-			}
-		} else {
-			let text_in = document.createTextNode(input)
+	function add() {
+		if (text.value) {
+			let li = document.createElement('li')
+			let div = document.createElement('div')
+			let img_check = document.createElement('img')
+			let p = document.createElement('p')
+			let text_in = document.createTextNode(text.value)
+			let img_delete = document.createElement('img')
+			img_check.classList.add('check')
+			img_delete.classList.add('delete')
+			lists.appendChild(li)
+			li.appendChild(div)
+			div.appendChild(img_check)
+			div.appendChild(p)
+			li.appendChild(img_delete)
+			img_check.src = 'img/check.png'
+			img_check.alt = 'Check logo'
+			img_delete.src = 'img/trash-can.png'
+			img_delete.alt = 'Trash logo'
 			p.appendChild(text_in)
+			text.value = ''
 		}
 		makeButtons()
 	}
-	add('Type and press "Enter" to make a list')
-	add('Click/tap the text to mark as done')
-	add('Click/tap again to unmark')
-	add('Click/tap the trach logo to remove the list')
 
 	function makeButtons() {
 		let li = Array.from(document.getElementsByTagName('li'))
